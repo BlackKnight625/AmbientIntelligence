86b47bf0951f918e032b5b0406da211763629690
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2022-03-24 15:49:09 +0000

    Successfully opening phone's camera

--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.cameraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
             if((error = intent.getStringExtra(ERROR_MESSAGE)) != null) {
                 ((TextView) findViewById(R.id.errorView)).setText(error);
             }
+        }
+
+        System.out.println("Camera App active!");
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 100);
         }
     }
 
--- CameraApp/app/src/main/java/com/example/cameraapp/SurveilanceActivity.java
+++ CameraApp/app/src/main/java/com/example/cameraapp/SurveilanceActivity.java
@@ -3,8 +3,9 @@ package com.example.cameraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.FrameLayout;
 
 import java.io.IOException;
 import java.net.Socket;
@@ -14,40 +15,36 @@ public class SurveilanceActivity extends AppCompatActivity {
     // Private attributes
 
     private Socket socket;
+    private Camera camera;
+    private CameraPreview preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surveilance);
 
+        if(safeCameraOpen()) {
+            preview = new CameraPreview(getApplicationContext(), camera);
+            ((FrameLayout) findViewById(R.id.cameraPreview)).addView(preview);
+        }
+
         //Thread that creates the socket
-        new Thread() {
+        /*new Thread() {
             @Override
             public void run() {
                 try {
+                    //Creating the socket
                     socket = new Socket(getIntent().getStringExtra(MainActivity.IP_MESSAGE),
                             Integer.parseInt(getIntent().getStringExtra(MainActivity.PORT_MESSAGE)));
                 } catch (IOException e) {
-                    runOnUiThread(() -> {
-                        Intent intent = new Intent(SurveilanceActivity.this, MainActivity.class);
-
-                        intent.putExtra(MainActivity.ERROR_MESSAGE,
-                                getString(R.string.error) + " " + e.getMessage());
-
-                        startActivity(intent);
-                    });
+                    //Something went wrong when creating the socket
+                    errorSendToMainScreen(getString(R.string.error) + " " + e.getMessage(), e);
                 } catch (NumberFormatException e) {
-                    runOnUiThread(() -> {
-                        Intent intent = new Intent(SurveilanceActivity.this, MainActivity.class);
-
-                        intent.putExtra(MainActivity.ERROR_MESSAGE,
-                                getString(R.string.error) + " " + getString(R.string.wrong_format) + " " + e.getMessage());
-
-                        startActivity(intent);
-                    });
+                    //The port was not well formatted
+                    errorSendToMainScreen(getString(R.string.error) + " " + getString(R.string.wrong_format) + " " + e.getMessage(), e);
                 }
             }
-        }.start();
+        }.start();*/
     }
 
     @Override
@@ -55,9 +52,49 @@ public class SurveilanceActivity extends AppCompatActivity {
         super.onDestroy();
 
         try {
-            socket.close();
+            if(socket != null) {
+                socket.close();
+            }
+
+            releaseCamera();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean safeCameraOpen() {
+        boolean qOpened = false;
+
+        try {
+            releaseCamera();
+            camera = Camera.open();
+            camera.setDisplayOrientation(90);
+            qOpened = (camera != null);
+        } catch (Exception e) {
+            errorSendToMainScreen(getString(R.string.error_open_camera) + " " + e.getMessage(), e);
+        }
+
+        return qOpened;
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private void errorSendToMainScreen(String errorText, Exception e) {
+        runOnUiThread(() -> {
+            Intent intent = new Intent(SurveilanceActivity.this, MainActivity.class);
+
+            intent.putExtra(MainActivity.ERROR_MESSAGE, errorText);
+
+            startActivity(intent);
+
+            if(e != null) {
+                e.printStackTrace();
+            }
+        });
     }
 }

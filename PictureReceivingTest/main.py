9d1cfbca03ac89e19717862360cedcbe6931c950
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2022-03-28 18:39:09 +0100

    Changed code in PictureReceivingTest to display the footage

--- PictureReceivingTest/main.py
+++ PictureReceivingTest/main.py
@@ -5,16 +5,22 @@ import grpc
 from concurrent import futures
 import communication_pb2_grpc as pb2_grpc
 import communication_pb2 as pb2
+import cv2
+import numpy as np
 
 class CameraToCentralSystemServiceService(pb2_grpc.CameraToCentralSystemServiceServicer):
     def __init__(self, *args, **kwargs):
         pass
 
     def send_footage(self, request, context):
-        bytes = request.picture
+        img_bytes = request.picture
         ts = request.time
+        img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), -1)
 
-        print("Received footage! Size: ", len(bytes))
+        cv2.imshow("Message", img)
+        cv2.waitKey(1)
+
+        #print("Received footage! Size: ", len(img_bytes))
 
         return pb2.FootageAck()
 

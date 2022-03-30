b42d90713d637572bf934bdb37507a30ef2c4770
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2022-03-30 13:26:50 +0100

    Added services to Central System server

--- CentralSystem/main.py
+++ CentralSystem/main.py
@@ -6,14 +6,15 @@ from concurrent import futures
 import communication_pb2_grpc as pb2_grpc
 import communication_pb2 as pb2
 import numpy as np
-
+import services
 
 
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    pb2_grpc.add_CameraToCentralSystemServiceServicer_to_server(CameraToCentralSystemServiceService(), server)
+    pb2_grpc.add_CameraToCentralSystemServiceServicer_to_server(services.CameraToCentralSystemServiceService(), server)
+    pb2_grpc.add_SmartphoneAppToCentralSystemServiceServicer_to_server(services.SmartphoneAppToCentralSystemService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()

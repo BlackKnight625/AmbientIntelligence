d53687bededb6641375414230dbee729e7b162f1
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2022-04-02 17:47:13 +0100

    completed services in smartphoneapptocentralsystemserviceservice

--- CentralSystem/itemsStorage.py
+++ CentralSystem/itemsStorage.py
@@ -1,4 +1,11 @@
+import sys
 
+sys.path.insert(1, '../GrpcContract/target')
+
+import communication_pb2 as pb2
+
+lock = "locked"
+track = "tracked"
 
 def loadItemsStorage():
     pass
@@ -7,24 +14,48 @@ def saveItemsStorage():
     pass
 
 class ItemsStorage:
+    def __init__(self):
+        self.dic = {}
 
     def inserItem(self, itemId, locked, tracked):
-        pass
+        #if itemId in self.dic:
+        #    raise Exception(pb2.PhotoResponse.ITEM_ALREADY_EXISTS)
+        self.dic[itemId] = {}
+        self.dic[itemId][track] = tracked
+        self.dic[itemId][lock] = locked
 
     def removeItem(self, itemId):
-        pass
+        del self.dic[itemId]
 
     def isTracked(self, itemId):
-        pass
+        return self.dic[itemId][track]
 
     def isLocked(self, itemId):
-        pass
+        return self.dic[itemId][lock]
 
-    def setTracked(self, itemId, isTracked):
-        pass
+    def setTracked(self, itemId, locked):
+        self.dic[itemId][track] = locked
 
-    def setLocked(self, itemId, isLocked):
-        pass
+    def setLocked(self, itemId, locked):
+        self.dic[itemId][lock] = locked
 
     def getAllItemsAsList(self):
-        pass
+        res = []
+        for i in self.dic:
+            item = [i] + list(self.dic[i].values())
+            res.append(item)
+        return res
+
+    def has_item(self, itemId):
+        return itemId in self.dic
+
+    def get_search_results(self, searchParameters):
+        res = []
+        items = [item.find(searchParameters) for item in list(self.dic.keys())]
+        ix = 0
+        for i in self.dic:
+            if items[ix] == 0:
+                item = [i] + list(self.dic[i].values())
+                res.append(item)
+            ix += 1
+        return res
--- CentralSystem/services.py
+++ CentralSystem/services.py
@@ -1,4 +1,6 @@
 import sys
+from GrpcContract.target.communication_pb2 import PhotoResponse
+from itemsStorage import ItemsStorage
 
 sys.path.insert(1, '../GrpcContract/target')
 
@@ -6,6 +8,15 @@ import communication_pb2_grpc as pb2_grpc
 import communication_pb2 as pb2
 import imageProcessing
 import footageStorage as fs
+
+classNames = []
+classFile = "../archive/coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+def getItemName(classId):
+    return classNames[classId - 1]
+
 
 """This class handles messages that were sent from the Surveillance Cameras (Camera App)"""
 class CameraToCentralSystemServiceService(pb2_grpc.CameraToCentralSystemServiceServicer):
@@ -33,33 +44,57 @@ class CameraToCentralSystemServiceService(pb2_grpc.CameraToCentralSystemServiceS
 """This class handles messages that were sent from the SmartPhone App"""
 class SmartphoneAppToCentralSystemService(pb2_grpc.SmartphoneAppToCentralSystemServiceServicer):
     def __init__(self, *args, **kwargs):
-        pass
+        self.iems_storage = ItemsStorage()
 
     def locateItem(self, itemID, context): # Returns VideoFootage
         id = itemID.id
         pass
 
     def photoTaken(self, footage, context): # Returns PhotoResponse
-        pass
+        img_bytes = footage.picture
+
+        items, _ = imageProcessing.processImage(imageProcessing.getImageFromBytes(img_bytes))
+        photoResponse = pb2.PhotoResponse() 
+        if len(items) == 0:
+            photoResponse.newItemId = ""
+            photoResponse.status = pb2.PhotoResponse.NO_ITEM_FOUND
+        elif len(items) > 1:
+            photoResponse.newItemId = ""
+            photoResponse.status = pb2.PhotoResponse.MULTIPLE_ITEMS_FOUND
+        elif self.items_storage.has_item(getItemName(items[0])):
+            photoResponse.newItemId = ""
+            photoResponse.status = pb2.PhotoResponse.ITEM_ALREADY_EXISTS
+        else:
+            photoResponse.newItemId = getItemName(items[0])
+            photoResponse.status = pb2.PhotoResponse.OK
+        return photoResponse
 
     def confirmItemInsertion(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.insertItem(itemID.id, True, False)
+        return pb2.Ack()
 
     def searchItem(self, searchParameters, context): # Returns SearchResponse
-        pass
+        searchResponse = pb2.SearchResponse()
+        searchResponse.searchResults = self.items_storage.get_search_results(searchParameters)
+        return searchResponse
 
     def trackItem(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.setTracked(itemID.id, True)
+        return pb2.Ack()
 
     def untrackItem(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.setTracked(itemID.id, False)
+        return pb2.Ack()
 
     def lockItem(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.setLocked(itemID.id, True)
+        return pb2.Ack()
 
     def unlockItem(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.setLocked(itemID.id, False)
+        return pb2.Ack()
 
     def removeItem(self, itemID, context): # Returns Ack
-        pass
+        self.items_storage.removeItem(itemID.id)
+        return pb2.Ack()
 

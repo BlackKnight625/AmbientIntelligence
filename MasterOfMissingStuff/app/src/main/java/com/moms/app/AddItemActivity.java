3eb96623b30c2909a03b615f1a0247ecebfe2ed2
miguel-lourenco <miguel.lourenco2000@hotmail.com>
2022-04-08 01:28:38 +0100

    Implemented save button functionallity

--- MasterOfMissingStuff/app/src/main/java/com/moms/app/AddItemActivity.java
+++ MasterOfMissingStuff/app/src/main/java/com/moms/app/AddItemActivity.java
@@ -2,11 +2,16 @@ package com.moms.app;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.RequiresApi;
@@ -19,11 +24,13 @@ import com.moms.app.grpc.observers.PhotoTakenObserver;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Calendar;
+import java.util.HashMap;
 
 import pt.tecnico.moms.grpc.Communication;
 
 public class AddItemActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private boolean photoTaken = false;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -34,6 +41,18 @@ public class AddItemActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
         getWindow().setStatusBarColor(ContextCompat.getColor(AddItemActivity.this,R.color.white));// set status background white
+
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        editText.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                if (photoTaken) {
+                    enableSaveButtonIfTitleExists();
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+
 
         final Button add_picture_button = findViewById(R.id.button5);
         add_picture_button.setOnClickListener(new View.OnClickListener() {
@@ -46,11 +65,17 @@ public class AddItemActivity extends AppCompatActivity {
         });
 
         final Button save_button = findViewById(R.id.button7);
+        save_button.setEnabled(false);
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Code here executes on main thread after user presses button
-                //startActivity(new Intent(getApplicationContext(), ItemActivity.class));
+                ImageView imageView = (ImageView) findViewById(R.id.imageView7);
+                Bitmap image = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] imageBytes = stream.toByteArray();
+                CentralSystemFrontend.FRONTEND.photoTaken(ByteString.copyFrom(imageBytes), Calendar.getInstance(), new PhotoTakenObserver(AddItemActivity.this));
             }
         });
 
@@ -77,19 +102,18 @@ public class AddItemActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-            //Compressing taken image
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 50, out);
-
-            byte[] compressedPicture = out.toByteArray();
-
-            //Sending image to Central System
-            CentralSystemFrontend.FRONTEND.photoTaken(ByteString.copyFrom(compressedPicture), Calendar.getInstance(), new PhotoTakenObserver(this));
-
             ImageView imageView = (ImageView)findViewById(R.id.imageView7);
             imageView.setImageBitmap(imageBitmap);
+            photoTaken = true;
+            enableSaveButtonIfTitleExists();
+        }
+    }
+
+    public void enableSaveButtonIfTitleExists() {
+        if (((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString().trim().length() > 0) {
+            findViewById(R.id.button7).setEnabled(true);
+        } else {
+            findViewById(R.id.button7).setEnabled(false);
         }
     }
 
@@ -106,6 +130,17 @@ public class AddItemActivity extends AppCompatActivity {
         switch (status) {
             case OK:
                 //TODO: Show the user the identified item's category
+                String name = ((EditText)findViewById(R.id.editTextTextPersonName)).getText().toString();
+                ImageView imageView = (ImageView) findViewById(R.id.imageView7);
+                Bitmap image = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                Item newItem = new Item(name, image, true, false, newItemId);
+
+                MainActivity.ITEMS.put(newItemId, newItem);
+                MainActivity.REQUEST_LOAD_ITEMS = true;
+
+                Intent intent = new Intent(getApplicationContext(), ItemActivity.class);
+                intent.putExtra("item", newItemId);
+                startActivity(intent);
                 break;
             case NO_ITEM_FOUND:
                 break;
